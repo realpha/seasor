1ff766cf874519cb9dc191b88483de39d1637ca8
Raphael Paier <raphael.paier@at.ey.com>
2022-12-16 02:35:49 +0100

    chore: clean up benchmarks and tests

--- _test_utils/utils.ts
+++ _test_utils/utils.ts
@@ -1,6 +1,56 @@
 import { fc } from "./deps.ts";
-import { binarySearchBy } from "../mod.ts";
-import { BoundComparator, Ordering, Predicate } from "../types/mod.ts";
+import { Ordering, Predicate } from "../types/mod.ts";
+
+export interface HasId {
+  id: number;
+}
+
+/**
+ * Predicates
+ */
+
+function isEqualId(lhs: HasId, rhs: HasId): boolean {
+  return lhs.id === rhs.id;
+}
+
+export function isEqualIdFactory(rhs: HasId) {
+  return (lhs: HasId) => isEqualId(lhs, rhs);
+}
+
+/**
+ * Comparators
+ */
+
+export function ascendingById(lhs: HasId, rhs: HasId): Ordering {
+  if (lhs.id < rhs.id) return Ordering.Less;
+  if (lhs.id > rhs.id) return Ordering.Greater;
+  return Ordering.Equal;
+}
+
+export function idComparatorFactory(rhs: HasId): (lhs: HasId) => Ordering {
+  return (lhs: HasId) => ascendingById(lhs, rhs);
+}
+
+/**
+ * fast-check Arbitraries
+ */
+
+export function generateArray() {
+  return fc.array(generateEntity(), { size: "medium" });
+}
+
+export function generateEntity() {
+  return fc.record(
+    {
+      id: fc.nat(),
+    },
+    { requiredKeys: ["id"] }
+  );
+}
+
+/**
+ * Misc
+ */
 
 export type JsType =
   | "bigint"
@@ -24,102 +74,3 @@ export const isNumber = isType.bind(null, "number");
 export function applyType(t: JsType) {
   return (v: unknown) => isType(t, v);
 }
-
-function isEqualIdPredicate(v: HasId, n: number): boolean {
-  return v.id === n;
-}
-
-export function isEqualIdFactory(idValue: number) {
-  return (v: HasId) => isEqualIdPredicate(v, idValue);
-}
-
-export function generateArray() {
-  return fc.array(generateHasIdElement(), { size: "medium" });
-}
-
-export function generateHasIdElement() {
-  return fc.record(
-    {
-      id: fc.nat(),
-    },
-    { requiredKeys: ["id"] }
-  );
-}
-
-export interface HasId {
-  id: number;
-}
-
-export function getIsEqualIdComperator(n: number): (v: HasId) => Ordering {
-  return ({ id }: HasId) => {
-    if (id < n) return Ordering.Less;
-    if (id > n) return Ordering.Greater;
-    return Ordering.Equal;
-  };
-}
-
-export function orderByIdComperator(a: HasId, b: HasId): Ordering {
-  if (a.id < b.id) return Ordering.Less;
-  if (a.id > b.id) return Ordering.Greater;
-  return Ordering.Equal;
-}
-
-function setupBinarySearch<T>(
-  arr: Array<T>,
-  comperator: BoundComparator<T>
-): () => void {
-  return () => {
-    binarySearchBy<T>(comperator)(arr);
-  };
-}
-
-function setupLinearSearch<T>(
-  arr: Array<T>,
-  comperator: (v: T) => boolean
-): () => void {
-  return () => {
-    arr.find(comperator);
-  };
-}
-
-function generateRandomSortedArray(len: number): Array<HasId> {
-  return Array(len)
-    .fill(1)
-    .map((v, i) => ({ id: Math.floor(Math.random() * 10) * (i + v) }))
-    .sort((a, b) => {
-      if (a.id < b.id) return Ordering.Less;
-      if (a.id > b.id) return Ordering.Greater;
-      return Ordering.Equal;
-    });
-}
-
-function generateRandomLookUpValue(len: number): number {
-  return Math.floor(Math.random() * len);
-}
-
-function generateDataSetup(len: number): [Array<HasId>, number] {
-  const arr = generateRandomSortedArray(len);
-  return [arr, arr[generateRandomLookUpValue(len)].id];
-}
-
-function generateSearchSetup(arr: Array<HasId>, lookUpValue: number) {
-  return {
-    binarySearch: setupBinarySearch<HasId>(
-      arr,
-      getIsEqualIdComperator(lookUpValue)
-    ),
-    linearSearch: setupLinearSearch<HasId>(arr, isEqualIdFactory(lookUpValue)),
-  };
-}
-
-const [TINY_ARRAY, TINY_LOOK_UP_VALUE] = generateDataSetup(10);
-const [SMALL_ARRAY, SMALL_LOOK_UP_VALUE] = generateDataSetup(100);
-const [MEDIUM_ARRAY, MEDIUM_LOOK_UP_VALUE] = generateDataSetup(1_000);
-const [LARGE_ARRAY, LARGE_LOOK_UP_VALUE] = generateDataSetup(100_000);
-const [XLARGE_ARRAY, XLARGE_LOOK_UP_VALUE] = generateDataSetup(10_000_00);
-
-const TINY = generateSearchSetup(TINY_ARRAY, TINY_LOOK_UP_VALUE);
-const SMALL = generateSearchSetup(SMALL_ARRAY, SMALL_LOOK_UP_VALUE);
-const MEDIUM = generateSearchSetup(MEDIUM_ARRAY, MEDIUM_LOOK_UP_VALUE);
-const LARGE = generateSearchSetup(LARGE_ARRAY, LARGE_LOOK_UP_VALUE);
-const XLARGE = generateSearchSetup(XLARGE_ARRAY, XLARGE_LOOK_UP_VALUE);
